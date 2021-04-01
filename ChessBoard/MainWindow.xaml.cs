43080f10ee945e510d3fc441411a153c3ff08fcd
Göni <simonf.goenitzer@gmail.com>
2021-04-01 11:25:57 +0200

    Initial commit

--- ChessBoard/MainWindow.xaml.cs
+++ ChessBoard/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 /**
- * @author ${Anas Al Kala, Simon Gönitzer}
+ * @author ${Fabian Falco, Simon Gönitzer}
  *
  */
 namespace ChessBoard
@@ -26,11 +15,7 @@ namespace ChessBoard
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Grid_ContextMenuClosing(object sender, ContextMenuEventArgs e)
-        {
-
+            
         }
     }
 }
